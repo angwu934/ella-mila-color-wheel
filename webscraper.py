1fd4f25a0343f0bfa406dd3f8a80a9c177b58d9e
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2022-07-22 21:58:36 -0400

    Commit w more images

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,17 @@
-print("hello world")
+from bs4 import BeautifulSoup
+
+with open("nailpolishcolors.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+tags = doc.find_all("img", {"class": "fade-in lazyloaded"})
+
+color_names = []
+
+for i in range(len(tags)):
+    name = tags[i].get("alt")
+    if name not in color_names and name != "":
+        color_names.append(name)
+
+print(color_names)
+print(type(tags))
+print(type(tags[5]))
