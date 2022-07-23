47a584670b5628c2271080b9f64ca7788847485e
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2022-07-22 21:02:47 -0400

    Added html website for color palette

---
+++ nailpolishcolors_files/pub_55e5cc71c57acbdba42d.js
@@ -0,0 +1,8 @@
+// Last edit: Fri, 22 Jul 22 20:47:35 -0400
+_rfsn_tracker.load_settings({
+	version : 2.0,
+	base_url : 'https://ellamila.refersion.com/',
+	xdomains: [],
+	verbose: false,
+	is_loaded: true
+})
