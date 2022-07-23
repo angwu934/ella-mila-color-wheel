300f8576c5ef07ee7c8b77188232bae4c11dade9
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2022-07-23 00:19:16 -0400

    One Collection BABY

--- webscraper.py
+++ webscraper.py
@@ -1,17 +1,39 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-with open("nailpolishcolors.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
+# with open("nailpolishcolors.html", "r") as f:
+#     doc = BeautifulSoup(f, "html.parser")
 
-tags = doc.find_all("img", {"class": "fade-in lazyloaded"})
+# tags = doc.find_all("img", {"class": "fade-in lazyloaded"})
 
-color_names = []
+# color_names = []
 
-for i in range(len(tags)):
-    name = tags[i].get("alt")
-    if name not in color_names and name != "":
-        color_names.append(name)
+# for i in range(len(tags)):
+#     name = tags[i].get("alt")
+#     if name not in color_names and name != "":
+#         color_names.append(name)
 
-print(color_names)
-print(type(tags))
-print(type(tags[5]))
+# print(color_names)
+# print(type(tags))
+# print(type(tags[5]))
+
+from bs4 import BeautifulSoup 
+import requests
+
+birthday_url = "https://www.ellamila.com/collections/nail-gifts-value-sets/products/birthday-collection"
+
+birthday_result = requests.get(birthday_url)
+birthday_doc = BeautifulSoup(birthday_result.text, "html.parser")
+
+
+birthday_parent = birthday_doc.find_all("span")
+colorstr = ""
+for i in range(len(birthday_parent)):
+    if "Color" in birthday_parent[i].text:
+        colorstr = birthday_parent[i].text
+        break
+
+print(colorstr)
+print(" ")
+print(colorstr.split(": ")[1].split(", "))
+
+    
